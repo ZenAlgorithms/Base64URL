de86718d149d4e4a213c3846e1bd45cfb119a724
Ian Torres <iantorres@outlook.com>
2024-10-25 11:54:57 -0300

    Base64 implemented.

--- lib/headers/base64url/container.hpp
+++ lib/headers/base64url/container.hpp
@@ -6,6 +6,13 @@
 #include <regex>
 
 namespace base64url {
+    static const std::string chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789-_";
+
     struct container {
+        std::string static encode(const std::string & input, bool padding = true);
+        std::string static decode(const std::string & input);
     };
 }
--- tests/implementation_test.cc
+++ tests/implementation_test.cc
@@ -1,6 +1,23 @@
 #include <gtest/gtest.h>
 #include <base64url/container.hpp>
 
-TEST(Base64URL, Assertions) {
+TEST(Base64URL, Encode) {
     using namespace base64url;
+    const std::string input = "hello world";
+    const std::string output = container::encode(input);
+    ASSERT_EQ(output, "aGVsbG8gd29ybGQ=");
+
+    const std::string output_no_padding = container::encode(input, false);
+    ASSERT_EQ(output_no_padding, "aGVsbG8gd29ybGQ");
+}
+
+TEST(Base64URL, Decode) {
+    using namespace base64url;
+    const std::string input = "aGVsbG8gd29ybGQ=";
+    const std::string output = container::decode(input);
+    ASSERT_EQ(output, "hello world");
+
+    const std::string input_no_padding = "aGVsbG8gd29ybGQ";
+    const std::string output_no_padding = container::decode(input_no_padding);
+    ASSERT_EQ(output_no_padding, "hello world");
 }
